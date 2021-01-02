3d2ee1b1ef30d4ae6043053d30af3899ab8f0709
Markus Frohme <markus.frohme@udo.edu>
2021-01-02 21:25:30 +0100

    some more experimentation

--- src/test/java/com/github/mtf90/AppTest.java
+++ src/test/java/com/github/mtf90/AppTest.java
@@ -4,8 +4,8 @@ import org.testng.annotations.Test;
 
 public class AppTest {
 
-    @Test
-    public void testApp() {
-        App.main(new String[0]);
-    }
+//    @Test
+//    public void testApp() {
+//        App.main(new String[0]);
+//    }
 }
